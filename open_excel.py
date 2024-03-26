5e47551fe6ce9540192a19d3793227fac1e9f2cb
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2024-03-26 15:06:41 +0100

    Openpyxl package imported to retreive excel files

--- open_excel.py
+++ open_excel.py
@@ -1,1 +1,8 @@
-welcome
+# we import our package we installed 
+from openpyxl import load_workbook
+
+# we load spreadsheet, the main object 
+wb = load_workbook('sample.xlsx')
+
+
+# we can start manipulating the data in cells 
